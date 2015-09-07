c34938ba7ff922b18dd47dc0b6332647e9945221
Johannes Schriewer <j.schriewer@anfe.ma>
2015-09-07 16:47:20 +0200

    Initial Commit

---
+++ amp-client/amp-client.h
@@ -0,0 +1,19 @@
+//
+//  amp-client.h
+//  amp-client
+//
+//  Created by Johannes Schriewer on 07.09.15.
+//  Copyright © 2015 anfema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for amp-client.
+FOUNDATION_EXPORT double amp_clientVersionNumber;
+
+//! Project version string for amp-client.
+FOUNDATION_EXPORT const unsigned char amp_clientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <amp_client/PublicHeader.h>
+
+
---
+++ amp-clientTests/amp_clientTests.swift
@@ -0,0 +1,36 @@
+//
+//  amp_clientTests.swift
+//  amp-clientTests
+//
+//  Created by Johannes Schriewer on 07.09.15.
+//  Copyright © 2015 anfema. All rights reserved.
+//
+
+import XCTest
+@testable import amp_client
+
+class amp_clientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
