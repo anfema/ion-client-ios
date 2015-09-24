d16348e865c594b545aca49a93dbbad8fd345846
Johannes Schriewer <j.schriewer@anfe.ma>
2015-09-24 17:53:38 +0200

    Add documentation to container

--- amp-client/model/content/container.swift
+++ amp-client/model/content/container.swift
@@ -13,6 +13,11 @@ import DEjson
 public class AMPContainerContent : AMPContentBase {
     var children:[AMPContent]!
     
+    /// Initialize container content object from JSON
+    ///
+    /// - Parameter json: `JSONObject` that contains serialized container content object
+    ///
+    /// Container content children can be accessed by subscripting the container content object
     override init(json:JSONObject) throws {
         try super.init(json: json)
         
@@ -31,6 +36,7 @@ public class AMPContainerContent : AMPContentBase {
         }
     }
     
+    /// Container content has a subscript for it's children
     subscript(index: Int) -> AMPContent? {
         guard self.children != nil && index < self.children!.count else {
             return nil
@@ -40,6 +46,12 @@ public class AMPContainerContent : AMPContentBase {
 }
 
 extension AMPPage {
+    
+    /// Fetch `AMPContent`-Array from named outlet
+    ///
+    /// - Parameter name: the name of the outlet
+    /// - Returns: Array of `AMPContent` objects if the outlet was a container outlet and the page was already
+    ///            cached, else nil
     public func children(name: String) -> [AMPContent]? {
         if let content = self.outlet(name) {
             if case .Container(let container) = content {
@@ -49,6 +61,12 @@ extension AMPPage {
         return nil
     }
     
+    /// Fetch `AMPContent`-Array from named outlet async
+    ///
+    /// - Parameter name: the name of the outlet
+    /// - Parameter callback: block to call when the children become available, will not be called if the outlet
+    ///                       is not a container outlet or non-existant or fetching the outlet was canceled because
+    ///                       of a communication error
     public func children(name: String, callback: ([AMPContent] -> Void)) {
         self.outlet(name) { content in
             if case .Container(let container) = content {
