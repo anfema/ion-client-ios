9ec20474ec1f176ec309fcdc79cd144d38090800
Johannes Schriewer <j.schriewer@anfe.ma>
2015-12-11 14:03:06 +0100

    Add test for page cache invalidation with mockingbird

--- amp-client-tests/tests/generic/auto_cache_invalidation.swift
+++ amp-client-tests/tests/generic/auto_cache_invalidation.swift
@@ -11,6 +11,7 @@
 
 import XCTest
 @testable import amp_client
+import mockingbird
 
 class autoCacheTests: LoggedInXCTestCase {
     
@@ -50,5 +51,66 @@ class autoCacheTests: LoggedInXCTestCase {
         AMP.config.cacheTimeout = 600
     }
 
+    
+    func testAutoPageUpdate() {
+        guard self.mock == true else {
+            return // only works with mocking enabled
+        }
+
+        // set default mock bundle
+        var path = NSBundle(forClass: self.dynamicType).resourcePath! + "/bundles/amp"
+        do {
+            try MockingBird.setMockBundle(path)
+        } catch {
+            XCTFail("Could not set up API mocking")
+        }
+
+        
+        // clear cache
+        AMP.resetDiskCache()
+        AMP.resetMemCache()
+        
+        let expectation = self.expectationWithDescription("testAutoPageUpdate1")
+
+        // seed cache
+        AMP.collection("test").download { success in
+            XCTAssert(success == true)
+            
+            expectation.fulfill()
+        }
+
+        self.waitForExpectationsWithTimeout(10.0, handler: nil)
+        let expectation2 = self.expectationWithDescription("testAutoPageUpdate2")
+
+        
+        // check page cache content
+        AMP.collection("test").page("page_001") { page in
+            XCTAssert(page.layout == "layout-001")
+            expectation2.fulfill()
+        }
+
+        self.waitForExpectationsWithTimeout(2.0, handler: nil)
+
+        // reset online update so the next call fetches the collection again
+        AMP.config.lastOnlineUpdate = nil
+        
+        let expectation3 = self.expectationWithDescription("testAutoPageUpdate3")
+
+        // Switch to other mock bundle
+        path = NSBundle(forClass: self.dynamicType).resourcePath! + "/bundles/amp_refresh"
+        do {
+            try MockingBird.setMockBundle(path)
+        } catch {
+            XCTFail("Could not set up API mocking")
+        }
+
+        // check if page has updated
+        AMP.collection("test").page("page_001") { page in
+            XCTAssert(page.layout == "new-layout-001")
+            expectation3.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(10.0, handler: nil)
+    }
     
 }
