d19595b786d093dda26860dc86a94716308cc8a0
Dominik Felber <d.felber@anfe.ma>
2016-03-01 16:44:10 +0100

    renewing sqlite connection for fts when updating fts db

---
+++ amp-client-tests/ios/amp-client/Notifications.swift
@@ -0,0 +1,9 @@
+//
+//  Notifications.swift
+//  amp-client
+//
+//  Created by Dominik Felber on 01.03.16.
+//  Copyright © 2016 anfema. All rights reserved.
+//
+
+internal let AMPFTSDBDidUpdateNotification = "amp_ftsdb_did_update_notification"
