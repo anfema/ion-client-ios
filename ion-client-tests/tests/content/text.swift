e0ffc6192c999adfd1aae2877e6b665f7b2e1816
Dominik Felber <d.felber@anfe.ma>
2016-04-22 16:25:07 +0200

    added some text outlet tests

--- ion-client-tests/tests/content/text.swift
+++ ion-client-tests/tests/content/text.swift
@@ -204,4 +204,293 @@ class textContentTests: LoggedInXCTestCase {
         
         self.waitForExpectationsWithTimeout(1.0, handler: nil)
     }
+    
+    
+    func testWrongOutletText() {
+        let expectation = self.expectationWithDescription("testWrongOutlet")
+        
+        ION.collection("test").page("page_001").text("number") { result in
+            guard case .Failure(let error) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletIncompatible = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testWrongOutletAttributedString() {
+        let expectation = self.expectationWithDescription("testWrongOutletAttributedString")
+        
+        ION.collection("test").page("page_001").attributedString("number") { result in
+            guard case .Failure(let error) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletIncompatible = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testWrongOutletHTML() {
+        let expectation = self.expectationWithDescription("testWrongOutletHTML")
+        
+        ION.collection("test").page("page_001").html("number") { result in
+            guard case .Failure(let error) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletIncompatible = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testWrongOutletText2() {
+        let expectation = self.expectationWithDescription("testWrongOutlet2")
+        
+        ION.collection("test").page("page_001").text("number", position: 1) { result in
+            guard case .Failure(let error) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletNotFound = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testWrongOutletAttributedString2() {
+        let expectation = self.expectationWithDescription("testWrongOutletAttributedString2")
+        
+        ION.collection("test").page("page_001").attributedString("number", position: 1) { result in
+            guard case .Failure(let error) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletNotFound = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testWrongOutletHTML2() {
+        let expectation = self.expectationWithDescription("testWrongOutletHTML2")
+        
+        ION.collection("test").page("page_001").html("number", position: 1) { result in
+            guard case .Failure(let error) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletNotFound = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testWrongOutletSync() {
+        let expectation = self.expectationWithDescription("testWrongOutletSync")
+        
+        ION.collection("test").page("page_001") { result in
+            guard case .Success(let page) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            let htmlResult = page.html("number")
+            
+            guard case .Failure(let htmlError) = htmlResult else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletIncompatible = htmlError else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            
+            let textResult = page.text("number")
+            
+            guard case .Failure(let textError) = textResult else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletIncompatible = textError else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            
+            let attributedStringResult = page.attributedString("number")
+            
+            
+            guard case .Failure(let attributedStringError) = attributedStringResult else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletIncompatible = attributedStringError else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testWrongOutletSync2() {
+        let expectation = self.expectationWithDescription("testWrongOutletSync2")
+        
+        ION.collection("test").page("page_001") { result in
+            guard case .Success(let page) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            let htmlResult = page.html("number", position: 1)
+            
+            guard case .Failure(let htmlError) = htmlResult else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletNotFound = htmlError else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            
+            let textResult = page.text("number", position: 1)
+            
+            guard case .Failure(let textError) = textResult else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletNotFound = textError else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            
+            let attributedStringResult = page.attributedString("number", position: 1)
+            
+            
+            guard case .Failure(let attributedStringError) = attributedStringResult else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletNotFound = attributedStringError else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testTextOutlet() {
+        let expectation = self.expectationWithDescription("testTextOutlet")
+        
+        ION.collection("test").page("page_001").outlet("text") { result in
+            guard case .Success(let outlet) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard let textOutlet = outlet as? IONTextContent else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            XCTAssertNotNil(textOutlet.plainText())
+            XCTAssertNotNil(textOutlet.htmlText())
+            XCTAssertNotNil(textOutlet.attributedString())
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
 }
