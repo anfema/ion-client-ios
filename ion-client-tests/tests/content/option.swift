ee0ea19fd3fa2d588b12cc1cdc073c83648f5629
Dominik Felber <d.felber@anfe.ma>
2016-04-25 09:52:39 +0200

    added tests for option content

--- ion-client-tests/tests/content/option.swift
+++ ion-client-tests/tests/content/option.swift
@@ -10,6 +10,7 @@
 // BSD license (see LICENSE.txt for full license text)
 
 import XCTest
+import DEjson
 @testable import ion_client
 
 class optionContentTests: LoggedInXCTestCase {
@@ -43,6 +44,7 @@ class optionContentTests: LoggedInXCTestCase {
         self.waitForExpectationsWithTimeout(1.0, handler: nil)
     }
     
+    
     func testOptionOutletFetchAsync() {
         let expectation = self.expectationWithDescription("testOptionOutletFetchAsync")
         
@@ -59,4 +61,173 @@ class optionContentTests: LoggedInXCTestCase {
         self.waitForExpectationsWithTimeout(1.0, handler: nil)
     }
 
+    
+    func testOutletIncompatible() {
+        let expectation = self.expectationWithDescription("testOutletIncompatible")
+        
+        ION.collection("test").page("page_001").selectedOption("number") { result in
+            guard case .Failure(let error) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletIncompatible = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testOutletIncompatibleSync() {
+        let expectation = self.expectationWithDescription("testOutletIncompatibleSync")
+        
+        ION.collection("test").page("page_001") { result in
+            guard case .Success(let page) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .Failure(let error) = page.selectedOption("number") else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletIncompatible = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testInvalidOutlet() {
+        let expectation = self.expectationWithDescription("testInvalidOutlet")
+        
+        ION.collection("test").page("page_001").selectedOption("wrong") { result in
+            guard case .Failure(let error) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletNotFound = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testInvalidOutletSync() {
+        let expectation = self.expectationWithDescription("testInvalidOutletSync")
+        
+        ION.collection("test").page("page_001") { result in
+            guard case .Success(let page) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .Failure(let error) = page.selectedOption("wrong") else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletNotFound = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testInvalidJSON() {
+        // invalid value for correct key
+        let json1: JSONObject = .JSONDictionary(["value": .JSONNumber(0.0)])
+        
+        do {
+            let _ = try IONOptionContent(json: json1)
+        }
+            
+        catch let e as IONError {
+            XCTAssertNotNil(e)
+            
+            guard case .InvalidJSON(let obj) = e else {
+                XCTFail("wrong error thrown")
+                return
+            }
+            
+            XCTAssertNotNil(obj)
+        } catch {
+            XCTFail("wrong error thrown")
+        }
+        
+        
+        // no "value" key
+        let json2: JSONObject = .JSONDictionary(["test": .JSONString("test")])
+        
+        do {
+            let _ = try IONOptionContent(json: json2)
+        }
+            
+        catch let e as IONError {
+            XCTAssertNotNil(e)
+            
+            guard case .InvalidJSON(let obj) = e else {
+                XCTFail("wrong error thrown")
+                return
+            }
+            
+            XCTAssertNotNil(obj)
+        } catch {
+            XCTFail("wrong error thrown")
+        }
+    }
+    
+    
+    func testJSONObjectExpected() {
+        let json: JSONObject = .JSONString("value")
+        
+        do {
+            let _ = try IONOptionContent(json: json)
+        }
+            
+        catch let e as IONError {
+            XCTAssertNotNil(e)
+            
+            guard case .JSONObjectExpected(let obj) = e else {
+                XCTFail("wrong error thrown")
+                return
+            }
+            
+            XCTAssertNotNil(obj)
+        } catch {
+            XCTFail("wrong error thrown")
+        }
+    }
 }
