357b5628976c4fdd0c72b438b257c03d2fc780c7
Dominik Felber <d.felber@anfe.ma>
2016-04-25 09:22:44 +0200

    added tests for flag content

--- ion-client-tests/tests/content/flag.swift
+++ ion-client-tests/tests/content/flag.swift
@@ -10,17 +10,12 @@
 // BSD license (see LICENSE.txt for full license text)
 
 import XCTest
+import DEjson
+import HashExtensions
 @testable import ion_client
 
+
 class flagContentTests: LoggedInXCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-    }
     
     func testFlagOutletFetchSync() {
         let expectation = self.expectationWithDescription("testFlagOutletFetchSync")
@@ -42,6 +37,7 @@ class flagContentTests: LoggedInXCTestCase {
         self.waitForExpectationsWithTimeout(1.0, handler: nil)
     }
     
+    
     func testFlagOutletFetchAsync() {
         let expectation = self.expectationWithDescription("testFlagOutletFetchAsync")
         
@@ -56,5 +52,175 @@ class flagContentTests: LoggedInXCTestCase {
             expectation.fulfill()
         }
         self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testOutletIncompatible() {
+        let expectation = self.expectationWithDescription("testOutletIncompatible")
+        
+        ION.collection("test").page("page_001").isSet("number") { result in
+            guard case .Failure(let error) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletIncompatible = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testOutletIncompatibleSync() {
+        let expectation = self.expectationWithDescription("testOutletIncompatibleSync")
+        
+        ION.collection("test").page("page_001") { result in
+            guard case .Success(let page) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .Failure(let error) = page.isSet("number") else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletIncompatible = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testInvalidOutlet() {
+        let expectation = self.expectationWithDescription("testInvalidOutlet")
+        
+        ION.collection("test").page("page_001").isSet("wrong") { result in
+            guard case .Failure(let error) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletNotFound = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testInvalidOutletSync() {
+        let expectation = self.expectationWithDescription("testInvalidOutletSync")
+        
+        ION.collection("test").page("page_001") { result in
+            guard case .Success(let page) = result else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .Failure(let error) = page.isSet("wrong") else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case .OutletNotFound = error else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    
+    func testInvalidJSON() {
+        // invalid value for correct key
+        let json1: JSONObject = .JSONDictionary(["is_enabled": .JSONString("wrong")])
+        
+        do {
+            let _ = try IONFlagContent(json: json1)
+        }
+            
+        catch let e as IONError {
+            XCTAssertNotNil(e)
+            
+            guard case .InvalidJSON(let obj) = e else {
+                XCTFail("wrong error thrown")
+                return
+            }
+            
+            XCTAssertNotNil(obj)
+        } catch {
+            XCTFail("wrong error thrown")
+        }
+        
+        
+        // no "is_enabled" key
+        let json2: JSONObject = .JSONDictionary(["test": .JSONString("test")])
+        
+        do {
+            let _ = try IONFlagContent(json: json2)
+        }
+            
+        catch let e as IONError {
+            XCTAssertNotNil(e)
+            
+            guard case .InvalidJSON(let obj) = e else {
+                XCTFail("wrong error thrown")
+                return
+            }
+            
+            XCTAssertNotNil(obj)
+        } catch {
+            XCTFail("wrong error thrown")
+        }
+    }
+    
+    
+    func testJSONObjectExpected() {
+        let json: JSONObject = .JSONString("is_enabled")
+        
+        do {
+            let _ = try IONFlagContent(json: json)
+        }
+            
+        catch let e as IONError {
+            XCTAssertNotNil(e)
+            
+            guard case .JSONObjectExpected(let obj) = e else {
+                XCTFail("wrong error thrown")
+                return
+            }
+            
+            XCTAssertNotNil(obj)
+        } catch {
+            XCTFail("wrong error thrown")
+        }
     }
 }
