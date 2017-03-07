7c9b8b38a6b6e047b8b2a52512d8ec073937dfc9
Dominik Felber <d.felber@anfe.ma>
2017-03-07 09:05:04 +0100

    added always block to AsyncResult

--- ion-client/communication/AsyncResult.swift
+++ ion-client/communication/AsyncResult.swift
@@ -17,13 +17,16 @@ import Foundation
 /// - Success: The operation and all post processing operations were successful resulting in the
 ///            provided associated value.
 /// - Failure: The request encountered an error resulting in a failure. The associated values is an error that caused the failure.
+/// - Finish:  Block that is called after the `success` or `failure` block.
 public class AsyncResult<T> {
-    
+
     private var success: ((T) -> Void)?
-    
+
     private var failure: ((Error) -> Void)?
 
-    
+    private var alwaysBlock: (() -> Void)?
+
+
     /// Handler that should be triggered on a successfull operation
     /// You can attach a onFailure handler right after the onSuccess handler
     @discardableResult public func onSuccess (_ success: @escaping (T) -> Void) -> AsyncResult {
@@ -31,27 +34,36 @@ public class AsyncResult<T> {
         return self
     }
 
-    
+
     /// Handler that should be triggered on a failed operation
     @discardableResult public func onFailure (_ failure: @escaping (Error) -> Void) -> AsyncResult {
         self.failure = failure
         return self
     }
 
-    
-    /// Executes the success or failure handler based on the provided Result
-    func execute(result: Result<T>) {
-        guard case .success(let value) = result else {
-            failure?(result.error ?? IONError.didFail)
-            return
-        }
 
-        success?(value)
+    /// Handler that should be triggered after `success` or `failure` was triggered
+    @discardableResult public func always (_ alwaysBlock: @escaping () -> Void) -> AsyncResult {
+        self.alwaysBlock = alwaysBlock
+        return self
     }
 
-    
+
+    /// Executes the success or failure handler based on the provided Result
+    func execute(result: Result<T>) {
+        if case .success(let value) = result {
+            success?(value)
+        } else {
+            failure?(result.error ?? IONError.didFail)
+        }
+
+        alwaysBlock?()
+    }
+
+
     deinit {
         success = nil
         failure = nil
+        alwaysBlock = nil
     }
 }
